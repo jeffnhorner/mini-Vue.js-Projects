b184b7cee0844135a3060714994d228e2ea4620f
Jeff Horner <jeffnhorner@outlook.com>
2018-12-27 12:48:52 -0600

    small change to Footer component so that the github repo would open in a new tab.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer>
-        <p>&copy; 2018 | <a href="https://www.jeffnhorner.com" target="_blank">Coded by Jeff Horner</a> | <a href="https://github.com/jhorner32/mini-Vue.js-Projects"><i class="fab fa-github"></i></a></p>
+        <p>&copy; 2018 | <a href="https://www.jeffnhorner.com" target="_blank">Coded by Jeff Horner</a> | <a href="https://github.com/jhorner32/mini-Vue.js-Projects" target="_blank"><i class="fab fa-github"></i></a></p>
     </footer>
 </template>
 
